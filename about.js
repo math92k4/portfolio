39b870fdeda9b593c262236a455c92f38e514a0e
Mathias <math92k4@stud.kea.dk>
2021-04-30 12:21:52 +0200

    burger menu fixed on all pages

--- about.js
+++ about.js
@@ -3,10 +3,12 @@
 import { scrollBarSetup } from "./scrollbar.js";
 import { cursorSetup } from "./cursor.js";
 import { headerFxSetup } from "./header_scroll_fx.js";
+import { constructMenu } from "./burger_menu.js";
 
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
+  constructMenu();
   cursorSetup();
   scrollBarSetup();
   headerFxSetup();
